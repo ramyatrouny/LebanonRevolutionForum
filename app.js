cd1a9f45041821a891b78cc532232f3c0319f481
mba3gar <ramiatrouni@gmail.com>
2019-10-20 10:34:21 +0300

    add heroku script

--- app.js
+++ app.js
@@ -23,7 +23,15 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, "client/build")));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
 
 dbConnect();
 
